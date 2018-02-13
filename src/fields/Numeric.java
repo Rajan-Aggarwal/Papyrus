3ef1b197f270df8c35e6f65a05e5b48493e90f61
Vishnu Dasu <vishnu98dasu@gmail.com>
2018-02-13 16:32:27 +0530

    Add Numeric class

--- src/fields/Numeric.java
+++ src/fields/Numeric.java
@@ -1,4 +1,15 @@
 package fields;
 
-public class Numeric {
+public class Numeric extends ColumnField {
+
+    double var;
+    private int size;
+    private int precision;
+
+    Numeric (int size, int precision, boolean ... args){ // nullable, primary, foreign, defValue
+
+        super(args);
+        this.size = size;
+        this.precision = precision;
+    }
 }
