4da27be03e88249d1db8b723c85c757f00ee8b8a
Vishnu Dasu <vishnu98dasu@gmail.com>
2018-02-13 16:32:09 +0530

    Add constructor

--- src/fields/ColumnField.java
+++ src/fields/ColumnField.java
@@ -1,13 +1,32 @@
 package fields;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
 /*
     Abstract class that must be extended by every other field class
  */
 
 public abstract class ColumnField {
 
-    public boolean nullable = true;
+    public boolean nullable = false;
     public boolean primary = false;
     public boolean foreign = false;
+    public boolean defValue = false;
 
+    ColumnField (boolean ... args) {
+
+        ArrayList<Boolean> varargs;
+        varargs = new ArrayList<>((Collection<? extends Boolean>) Arrays.asList(args));
+
+        while(varargs.size()<4) {
+            varargs.add(false);
+        }
+
+        nullable = varargs[0];
+        primary = varargs[1];
+        foreign = varargs[2];
+        defValue = varargs[3];
+    }
 }
---
+++ src/fields/Numeric.java
@@ -0,0 +1,4 @@
+package fields;
+
+public class Numeric {
+}
