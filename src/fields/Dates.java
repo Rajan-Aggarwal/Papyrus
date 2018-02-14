322b69c991ab928c07d84cece400bc366d03b4e7
Vishnu Dasu <vishnu98dasu@gmail.com>
2018-02-14 18:52:04 +0530

    Add Date class

--- src/fields/Dates.java
+++ src/fields/Dates.java
@@ -1,4 +1,15 @@
 package fields;
 
-public class Dates {
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Dates extends ColumnField {
+
+    Date date;
+
+    Dates (String date, boolean ... args) {
+
+        super(args);
+        this.date = new SimpleDateFormat("YYYY-MM-DD").parse(date);
+    }
 }
