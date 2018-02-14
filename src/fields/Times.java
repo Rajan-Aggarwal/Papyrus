a3be8a66aa256019e11a25a952105d1c21afcbb6
Vishnu Dasu <vishnu98dasu@gmail.com>
2018-02-14 18:55:31 +0530

    Modify function signature

--- src/fields/Times.java
+++ src/fields/Times.java
@@ -1,5 +1,6 @@
 package fields;
 
+import java.text.ParseException;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
@@ -7,7 +8,7 @@ public class Times extends ColumnField {
 
     Date time;
 
-    Times (String time, boolean ... args){
+    Times (String time, boolean ... args) throws ParseException {
 
         super(args);
         this.time = new SimpleDateFormat("hh:mm:ss").parse(time);
