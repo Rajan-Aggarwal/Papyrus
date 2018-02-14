17c9501394fdcc9abf559aef1646ba523485689c
Vishnu Dasu <vishnu98dasu@gmail.com>
2018-02-14 18:52:17 +0530

    Add Time class

--- src/fields/Times.java
+++ src/fields/Times.java
@@ -1,4 +1,15 @@
 package fields;
 
-public class Times {
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Times extends ColumnField {
+
+    Date time;
+
+    Times (String time, boolean ... args){
+
+        super(args);
+        this.time = new SimpleDateFormat("hh:mm:ss").parse(time);
+    }
 }
