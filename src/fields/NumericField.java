854c29626d1c327c6a253ea40160d0368c99653a
Vishnu Dasu <vishnu98dasu@gmail.com>
2018-02-26 23:15:22 +0530

    Add field name

--- src/fields/NumericField.java
+++ src/fields/NumericField.java
@@ -3,22 +3,25 @@ package fields;
 public class NumericField extends ColumnField {
 
     private double defaultValue;
+    private String fieldName;
     private int size;
     private int precision;
 
-    public NumericField(int size, int precision, boolean ... args) { // nullable, primary, foreign, defValue
+    public NumericField(String fieldName, int size, int precision, boolean ... args) { // nullable, primary, foreign, defValue
 
         super(args);
         this.size = size;
+        this.fieldName = fieldName;
         this.precision = precision;
         this.defaultValue = Double.NaN;
 
     }
 
-    public NumericField(int size, int precision, double defaultValue, boolean ... args){
+    public NumericField(String fieldName, int size, int precision, double defaultValue, boolean ... args){
 
         super(args);
         this.size = size;
+        this.fieldName = fieldName;
         this.precision = precision;
         this.defaultValue = defaultValue;
 
@@ -37,5 +40,10 @@ public class NumericField extends ColumnField {
     public int getPrecision () {
 
         return precision;
+    }
+
+    public String getFieldName() {
+
+        return fieldName;
     }
 }
