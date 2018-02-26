9837ff7059292f2f9b80c4fd12069d06fe5e4159
Vishnu Dasu <vishnu98dasu@gmail.com>
2018-02-26 23:53:50 +0530

    Remove unecessary comments

--- src/fields/ColumnField.java
+++ src/fields/ColumnField.java
@@ -1,9 +1,5 @@
 package fields;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-
 /*
     Abstract class that must be extended by every other field class
  */
@@ -28,34 +24,6 @@ public abstract class ColumnField {
                 }
             }
         }
-
-        //ArrayList<Boolean> varargs = new ArrayList<Boolean>();
-
-        /*for (int i=0; i<args.length; i++) {
-            varargs.add(args[i]);
-        }
-
-        /*while(varargs.size()<2) { //adds false to all the fields till the size becomes 3
-            //except the first one, which should be true by default
-            if (varargs.size()==1) {
-                varargs.add(true);
-            }
-            else {
-                varargs.add(false);
-            }
-        }*/
-
-        /*if (varargs.size()==1) {
-            isNullable = varargs.get(0);
-        } else {
-            isNullable = varargs.get(0);
-            isPrimary = varargs.get(1);
-        }*/
-
-        //System.out.println("nullable" + isNullable + "primary" + isPrimary);
-
-        //isForeign = (boolean) varargs.get(2);s
-
     }
 
     public boolean isNullable() {
