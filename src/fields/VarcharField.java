8cedbbde0440645ecc5b06b5d38be06842855e4d
Vishnu Dasu <vishnu98dasu@gmail.com>
2018-02-26 23:15:13 +0530

    Add field name

--- src/fields/VarcharField.java
+++ src/fields/VarcharField.java
@@ -3,19 +3,22 @@ package fields;
 public class VarcharField extends ColumnField {
 
     private String defaultText;
+    private String fieldName;
     private int size; //necessary
 
-    public VarcharField(int size, boolean ... args)  {
+    public VarcharField(String fieldName, int size, boolean ... args)  {
 
         super(args);
         this.size = size;
+        this.fieldName = fieldName;
         this.defaultText = ""; //see Mapper implementation
 
     }
 
-    public VarcharField(int size, String defaultText, boolean ... args) {
+    public VarcharField(String fieldName, int size, String defaultText, boolean ... args) {
 
         super(args);
+        this.fieldName = fieldName;
         this.size = size;
         this.defaultText = defaultText;
 
@@ -29,6 +32,11 @@ public class VarcharField extends ColumnField {
     public int getSize() {
 
         return size;
+    }
+
+    public String getFieldName() {
+
+        return fieldName;
     }
 
 }
