5e4dfff74153dfd6d2c5c82bb8599f909ebeb4b5
Vishnu Dasu <vishnu98dasu@gmail.com>
2018-02-26 23:16:34 +0530

    Convert to string type

--- src/fields/DateField.java
+++ src/fields/DateField.java
@@ -8,24 +8,35 @@ import java.text.SimpleDateFormat;
 
 public class DateField extends ColumnField {
 
-    private Date defaultDate;
+    private String defaultDate;
+    private String fieldName;
 
-    public DateField(boolean ... args) throws ParseException {
-
-        super(args);
-        String dateStamp = new SimpleDateFormat("YYYY-MM-DD").format(Calendar.getInstance().getTime());
-        DateFormat df = new SimpleDateFormat("YYYY-MM-DD");
-        this.defaultDate = df.parse(dateStamp);
-    }
-
-    public DateField(String defaultDate, boolean ... args) throws ParseException {
+    public DateField(String fieldName, boolean ... args) throws ParseException {
 
         super(args);
-        this.defaultDate = new SimpleDateFormat("YYYY-MM-DD").parse(defaultDate);
+        this.fieldName = fieldName;
+        this.defaultDate = new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
     }
 
-    public Date getDate() {
+    public DateField(String fieldName, String defaultDate, boolean ... args) throws ParseException {
 
-        return this.defaultDate;
+        super(args);
+        this.fieldName = fieldName;
+        this.defaultDate = new SimpleDateFormat("yyyy-MM-dd").format(defaultDate);
+    }
+
+    public String getDate() {
+
+        return defaultDate;
+    }
+
+    public String getFieldName() {
+
+        return fieldName;
+    }
+
+    public String getDefaultValue() {
+
+        return defaultDate.toString();
     }
 }
