9472eade622e291a3ced8d825b90006861b905e5
Vishnu Dasu <vishnu98dasu@gmail.com>
2018-02-26 23:14:19 +0530

    Test date and time classes

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,14 +1,27 @@
 package test;
 
 import fields.ColumnField;
+import fields.DateField;
+import fields.TimeField;
 import fields.VarcharField;
 import mapper.InvalidFieldException;
 import mapper.Mapper;
 
 import java.lang.reflect.Field;
+import java.sql.Time;
+import java.text.ParseException;
 
 class Student {
-    VarcharField name = new VarcharField(10, "Rajan", false, true, true);
+
+    TimeField name;
+
+    {
+        try {
+            name = new TimeField("DOB", false, true, true);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 public class Test {
